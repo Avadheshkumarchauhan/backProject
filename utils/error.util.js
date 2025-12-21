921a604b14c716f4d7d82ddabea1d2ca47f76eb4
Avadheshkumarchauhan <avadheaha@gmail.com>
2025-12-21 17:53:52 +0530

    fix: ensure success property is set to false in AppError class

--- utils/error.util.js
+++ utils/error.util.js
@@ -2,6 +2,7 @@ class AppError extends Error{
     constructor(messege,statusCode){
         super(messege);
         this.statusCode = statusCode;
+        this.success = false;
         Error.captureStackTrace(this,this.constructor)
     }
 }
