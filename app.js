83d0facc51d46fd45eb2d34f625b8941dc619d11
Avadheshkumarchauhan <avadheaha@gmail.com>
2026-01-22 01:12:55 +0530

    Comment out unused root route handler in app.js

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(morgan('dev'))
-app.get('/',(req,res)=>{
-    res.send('<h1>Hello </h1>')
-});
+// app.get('/',(req,res)=>{
+//     res.send('<h1>Hello </h1>')
+//});
+app.use(express.static("dist"));
 app.use('/pong',(req,res)=>{
     res.send('<h1>Pong </h1>')
 });
