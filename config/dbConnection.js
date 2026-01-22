02971f5d60e4531bbe558854f7caeedde89f925e
Avadheshkumarchauhan <avadheaha@gmail.com>
2026-01-22 15:51:58 +0530

    update

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import { config } from "dotenv";
+import { databaseName } from "../database.js";
 config({quiet:true})
 mongoose.set('strictQuery',false);
  const connectionToDb = async() => {
     try{
         const {connection} =await mongoose.connect(
-            process.env.MONGO_URL ||'mongodb://127.0.0.1:27017/website',{
+            `${process.env.MONGO_URL}${databaseName}`||'mongodb://127.0.0.1:27017/website',{
                 serverSelectionTimeoutMS: 30000
             }
         );
