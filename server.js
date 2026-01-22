a97532696a037f952b0f642087813ceff2b301c9
Avadheshkumarchauhan <avadheaha@gmail.com>
2026-01-22 17:09:31 +0530

    update

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ import connectionToDB from './config/dbConnection.js'
 dotenv.config({quiet:true});
 const PORT = process.env.PORT ||5000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+ await connectionToDB()
 app.listen(PORT,async()=>{
-    await connectionToDB()
+   
     console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
     
 });
